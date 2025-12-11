be8e22cb2b112e79fdf713df7818317d2fdd3444
Shinaayomi Adebisi <shinaayomiadebisi@gmail.com>
2025-12-11 14:58:18 -0800

    FOreach loop

--- Loops-C-Sharp/Program.cs
+++ Loops-C-Sharp/Program.cs
@@ -1,48 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("------------------- Loops ----------------------");
-Console.WriteLine("----------------- While Loop -----------------");
-
-Console.WriteLine("Show all the numbers from 1 to 10");
-var number = 1;
-while (number <= 10)
+﻿internal class Program
 {
-  Console.WriteLine(number);
-  number++;//number+=1
-}
+  private static void Main(string[] args)
+  {
+    Console.WriteLine("------------------- Loops ----------------------");
+    Console.WriteLine("----------------- While Loop -----------------");
 
-Console.WriteLine("Show all the odd numbers from 1 to 10");
- number = 1;
-while (number <= 10)
-{
-  Console.WriteLine(number);
-  number += 2;
-}
+    Console.WriteLine("Show all the numbers from 1 to 10");
+    var number = 1;
+    while (number <= 10)
+    {
+      Console.WriteLine(number);
+      number++;//number+=1
+    }
 
-Console.WriteLine("----------------- do..while Loop -----------------");
-number = 1;
-do
-{
-  Console.WriteLine(number);
-  number ++;
-} while (number <= 10);
+    Console.WriteLine("Show all the odd numbers from 1 to 10");
+    number = 1;
+    while (number <= 10)
+    {
+      Console.WriteLine(number);
+      number += 2;
+    }
 
-Console.WriteLine("-----------------------------------------");
-Console.WriteLine("----------------- for Loop -----------------");
-for (int i = 1; i <= 10; i++)
-{
-  Console.WriteLine(i);
-}
+    Console.WriteLine("----------------- do..while Loop -----------------");
+    number = 1;
+    do
+    {
+      Console.WriteLine(number);
+      number++;
+    } while (number <= 10);
 
-Console.WriteLine("Initialization omitted");
-number = 19;
-for (; number >= 10; number--)
-{
-  Console.WriteLine(number);
-}
+    Console.WriteLine("-----------------------------------------");
+    Console.WriteLine("---------------- for Loop ----------------");
+    for (int i = 1; i <= 10; i++)
+    {
+      Console.WriteLine(i);
+    }
 
-Console.WriteLine("Odd numbers using for loop");
-for (int counter = 1; counter <= 10; counter+=2)
-{
-  Console.WriteLine(counter);
+    Console.WriteLine("Initialization omitted");
+    number = 19;
+    for (; number >= 10; number--)
+    {
+      Console.WriteLine(number);
+    }
+
+    Console.WriteLine("Odd numbers using for loop");
+    for (int counter = 1; counter <= 10; counter += 2)
+    {
+      Console.WriteLine(counter);
+    }
+
+    Console.WriteLine("-----------------------------------------");
+    Console.WriteLine("----------------foreach Loop----------------");
+    var names = new string[] { "John", "David", "Sarah", "Gary" };
+    foreach (string name in names)
+    {
+      Console.WriteLine(name);
+    }
+
+    for (int index = 0; index < names.Length; index++)
+    {
+      string name = names[index];
+      Console.WriteLine(name);
+    }
+  }
 }
--- Loops-C-Sharp/obj/Debug/net10.0/Loops-C-Sharp.AssemblyInfo.cs
+++ Loops-C-Sharp/obj/Debug/net10.0/Loops-C-Sharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Loops-C-Sharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21db62af044572ec6dec01bc3319f302973e6bb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Loops-C-Sharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Loops-C-Sharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
